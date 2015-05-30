c025812f2b05cd0eebe7f26c275b313a5e69b355
aarjay00 <jain.rohit625@gmail.com>
2015-05-30 16:03:47 +0530

    adding data folder

---
+++ data/get_ssf_files.sh
@@ -0,0 +1,23 @@
+rm -rf ssf/*
+for directory in ann/* ;
+do
+	new_directory="$(echo $directory | cut -d'/' -f2)"
+	echo $directory
+	mkdir -p "ssf/"$new_directory
+	for files in $directory/*;
+	do
+		file="$(echo $files | cut -d'/' -f3 | cut -d'_' -f3)"
+		ssf_file="$(tree -fi ../hdtb/HDTB_pre_release_version-0.03/IntraChunk/SSF/utf/  | grep $file  )"
+		filename="$(echo $files | cut -d'/' -f3)"
+		if [ "$ssf_file" == "" ]
+		then
+			echo "original file name "$files
+			echo "searching for file num" $file
+			echo $filename" ssf file not present"
+			continue
+		fi
+#		echo $file
+#		echo $ssf_file
+		cp $ssf_file "ssf/"$new_directory/$filename
+	done
+done 
