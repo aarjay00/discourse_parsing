d94dbf69f09b1da5dd0c4e7d7d7557d0eaadcc36
aarjay00 <jain.rohit625@gmail.com>
2015-06-02 04:26:44 +0530

    ssf parsing api and minor changes

--- baseline/ssf_api.py
+++ baseline/ssf_api.py
@@ -4,16 +4,73 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+import re
 import ssf
 from util import *
+from bs4 import BeautifulSoup
+
+class SSFSentence():
+	def __init__(self):
+#		print "-"*30,"adding sentence"
+		self.chunkList=[]
+		self.wordList=[]
+		return
+	def addChunk(self,chunk):
+#		print "-"*30,"adding chunk"
+		self.chunkList.append(chunk)
+	def addWord(self,word):
+#		print "-"*30,"adding word to sentence"
+		self.wordList.append(word)
+
+class Chunk():
+	def __init__(self,tag,features_string):
+#		print "-"*30,"new chunk with tag - %s and f = %s"%(tag,features_string)
+		self.chunkTag=tag
+		self.featureSet=features_string
+		self.wordList=[]
+	def addWord(self,word):
+#		print "-"*30,"adding word to chunk"
+		self.wordList.append(word)
+class Word():
+	def __init__(self,word,tag,features_string):
+#		print "-"*30,"new word- %s with tag - %s and f = %s"%(word,tag,features_string)
+		self.wordTag=tag;
+		self.word=word
+		self.featureSet=features_string
 
 def extractSSFannotations(filePath):
 	filePath=filePath.replace("/raw/","/ssf/")
 	if not fileExists(filePath) :
 		print "No file found"
 		return None
-	node =ssf.node()
-	print "ssf from " , filePath,os.path.isfile(filePath)
-	node.read_ssf_from_file(filePath)
-	
+	fileFD=open(filePath,"r")
+	data=fileFD.read()
+	fileFD.close()
+	beautData = BeautifulSoup(data)
+	sentenceList=beautData.find_all('sentence')
+	SSFSentenceInstList=[]
+	for sentence in sentenceList:
+		SSFSentenceInst=SSFSentence()
+		content=sentence.renderContents()
+		lines=re.split("\n",content)
+		chunkInst=None
+		wordInst=None
+		for line in lines:
+			columns=line.split("\t")
+#			print line,"--",len(columns)
+			if(len(columns)<2): #useless line
+				continue
+			if(columns[1]=="(("): # new chunk
+				chunkInst=Chunk(columns[2],columns[3])
+			elif(columns[1]=="))"):
+				SSFSentenceInst.addChunk(chunkInst)
+			else:
+				wordInst=Word(columns[1],columns[2],columns[3])
+				chunkInst.addWord(wordInst)
+				SSFSentenceInst.addWord(wordInst)
+#		print "final stats",len(SSFSentenceInst.wordList),len(SSFSentenceInst.chunkList)
+		SSFSentenceInstList.append(SSFSentenceInst)
+	return SSFSentenceInstList
+
+#extractSSFannotations("./sample.ssf")	
 
