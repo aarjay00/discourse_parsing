4d556aba240a0bc985f75fa67e34286d671076b1
aarjay00 <jain.rohit625@gmail.com>
2015-06-04 06:22:44 +0530

    incomplete code to merge SSf and discourse annotations

--- baseline/main.py
+++ baseline/main.py
@@ -18,16 +18,99 @@ class discourseUnit():
 		self.spanEnd=span2
 		self.wordList=re.split(' ',unit)
 class discourseFile():
-	def __init__(self,sentenceList):
+	def __init__(self,sentenceList,globalWordList,rawData):
+		self.rawData=rawData
 		self.sentenceList=sentenceList
-	def addDiscourseRelationInfo(self,relationInfo):
-		pass
+		self.globalWordList=globalWordList
+	def addDiscourseRelationInfo(self,relationList):
+		self.relationList=relationList
+		for relation in self.relationList:
+			if(relation.relationType != "Explicit"):
+				continue
+			print relation.relationType
+			print "arg1",relation.arg1Span
+			print "connective",relation.connSpan
+			print "arg2",relation.arg2Span
+			span=convertSpan(relation.arg2Span,self.rawData,self.globalWordList)
+			printSpan(relation.arg2Span,self.rawData)
+			printSpanWord(span,self.globalWordList)
+		i=0
+		for word in self.globalWordList:
+			print word.word,i,
+			i+=1
+		i=0
+		startString=self.globalWordList[0].word
+		for i in range(1,5):
+			startString=startString+" "+globalWordList[i].word
+		data=self.rawData.replace(fullStop," "+fullStop+" ")
+		startDoc=data.find(startString)
+		data=data[startDoc:]
+		data=re.split('\s+',data)
+		print "-"*30
+		i=0
+		for word in data:
+			print word,i,
+			i+=1
+
 #	def addUnit(self,discourseUnit):
 #		self.discourseUnitList.append(discourseUnit)
 #	def addDiscourseRelationList(self,discourseRelationList):
 #		self.discourseRelationList=discourseRelationList
 #	def addSSFData(self,ssf):
 #		self.SSFData=ssf
+
+def printSpan(spans,rawData):
+	spans=re.split(';',spans)
+	for span in spans:
+		span=re.split("\.\.",span)
+		print span
+		print rawData[int(span[0]):int(span[1])],"-"
+def printSpanWord(spans,wordList):
+	for span in spans:
+		print span[0],span[1]
+		for word in wordList[span[0]:span[1]]:
+			print word.word,
+		print "-"
+
+def convertSpan(spans,rawData,wordList):
+	startString=wordList[0].word
+	for i in range(1,5):
+		startString=startString+" "+wordList[i].word
+	startDoc=rawData.find(startString)
+	spans=re.split(';',spans)
+	returnSpan=[]
+	for span in spans:
+		span=re.split("\.\.",span)
+#		print rawData[int(span[0]):int(span[1])]
+		dataBefore=rawData[startDoc:int(span[0])]
+		dataAfter=rawData[startDoc:int(span[1])]
+		dataBefore=dataBefore.replace(fullStop," "+fullStop+" ")
+		dataAfter=dataAfter.replace(fullStop," "+fullStop+" ")
+#		print "dataBefore starts--",len(re.split('\s+',dataBefore))
+#		i=0
+#		for w in re.split(' ',dataBefore):
+#			print w,i,
+#			i+=1
+#		print "dataBefore ends--"
+		returnSpan.append((int(len(re.split('\s+',dataBefore)))-1,int(len(re.split('\s+',dataAfter)))))
+	return returnSpan
+
+		
+def getSpan(span,data):
+	finalSpan=[]
+	spans=(span.split(";"))
+	for span  in spans:
+		span=span.split("..")
+		finalSpan.append(re.split(' ',data[int(span[0]):int(span[1])]))
+	return finalSpan
+def findListinList(list1 , list2):
+	for i in range(0,len(list1)):
+		for j in range(0,len(list2)):
+			if(list1[i+j].word!=list2[j]):
+				break
+		if(list1[i+j].word==list2[j]):
+			return i
+	return -1
 
 
 def processRawFile(data,connList,connSplitList,extraDelimiters):
@@ -55,10 +138,14 @@ connList=loadConnList("connectives/compConnectiveList.list")
 connSplitList=loadConnList("connectives/splitConnectiveList.list",True)
 discourseFileCollection=[]
 for rawFile in rawFileList:
-#	fd=codecs.open(rawFile,"r",encoding="utf-8")
+	fd=codecs.open(rawFile,"r",encoding="utf-8")
 #	discourseFileInst=processRawFile(fd.read(),connList,connSplitList,[])
-	sentenceList=extractSSFannotations(rawFile)
-	discourseFileInst=discourseFile(sentenceList)
+	(sentenceList,globalWordList)=extractSSFannotations(rawFile)
+	if(sentenceList==None or globalWordList==None):
+		print "Continuing"
+		continue
+	discourseFileInst=discourseFile(sentenceList,globalWordList,fd.read())
 	discourseFileInst.addDiscourseRelationInfo(extractRelation(rawFile))
 	discourseFileCollection.append(discourseFileInst)
-#	fd.close()
+	fd.close()
+	break
--- baseline/ssf_api.py
+++ baseline/ssf_api.py
@@ -15,30 +15,36 @@ class Sentence():
 	def __init__(self):
 #		print "-"*30,"adding sentence"
 		self.chunkList=[]
-		self.wordList=[]
+		self.wordNumList=[]
 		return
 	def addChunk(self,chunk):
 #		print "-"*30,"adding chunk"
 		self.chunkList.append(chunk)
 	def addWord(self,word):
 #		print "-"*30,"adding word to sentence"
-		self.wordList.append(word)
+		self.wordNumList.append(word)
 
 class Chunk():
-	def __init__(self,tag,features_string):
+	def __init__(self,tag,features_string,sentenceNum):
 #		print "-"*30,"new chunk with tag - %s and f = %s"%(tag,features_string)
 		self.chunkTag=tag
 		self.featureSet=FeatureSet(features_string)
 		self.wordNumList=[]
+		self.sentenceNum=sentenceNum
 	def addWord(self,word):
 #		print "-"*30,"adding word to chunk"
 		self.wordNumList.append(word)
 class Word():
-	def __init__(self,word,tag,features_string):
+	def __init__(self,word,tag,features_string,sentenceNum,chunkNum):
 #		print "-"*30,"new word- %s with tag - %s and f = %s"%(word,tag,features_string)
 		self.wordTag=tag;
 		self.word=word
 		self.featureSet=FeatureSet(features_string)
+		self.sentenceNum=sentenceNum
+		self.chunkNum=chunkNum
+		self.conn=False
+		self.arg1=False
+		self.arg2=False
 class FeatureSet():
 	def __init__(self,featureString):
 		self.featureDict={}
@@ -58,34 +64,48 @@ def extractSSFannotations(filePath):
 	filePath=filePath.replace("/raw/","/ssf/")
 	if not fileExists(filePath) :
 		print "No file found"
-		return None
+		return (None,None)
 	fileFD=codecs.open(filePath,"r",encoding="utf-8")
 	data=fileFD.read()
 	fileFD.close()
 	beautData = BeautifulSoup(data)
 	sentenceList=beautData.find_all('sentence')
-	SentenceInstList=[]
+	sentenceInstList=[]
+	globalWordList=[]
+	sentenceNum=0
+	wordNum=0
 	for sentence in sentenceList:
-		SentenceInst=Sentence()
+		sentenceInst=Sentence()
 		content=sentence.renderContents()
 		lines=re.split("\n",content)
 		chunkInst=None
 		wordInst=None
-		wordNum=0
+		chunkNum=-1
+		skip=False
 		for line in lines:
 			columns=line.split("\t")
 #			print line,"--",len(columns)
 			if(len(columns)<2): #useless line
 				continue
 			if(columns[1]=="(("): # new chunk
-				chunkInst=Chunk(columns[2],columns[3])
+				if("NULL" in columns[2]):
+					skip=True
+					continue
+				chunkNum+=1
+				chunkInst=Chunk(columns[2],columns[3],sentenceNum)
 			elif(columns[1]=="))"):
-				SentenceInst.addChunk(chunkInst)
+			  	if(skip==True):
+					sentenceInst.addChunk(chunkInst)
+				skip=False
 			else:
-				wordInst=Word(columns[1],columns[2],columns[3])
+			  	if(skip):
+					continue
+				wordInst=Word(columns[1],columns[2],columns[3],sentenceNum,chunkNum)
 				chunkInst.addWord(wordNum)
 				wordNum+=1
-				SentenceInst.addWord(wordInst)
+				sentenceInst.addWord(wordNum)
+				globalWordList.append(wordInst)
 #		print "final stats",len(SSFSentenceInst.wordList),len(SSFSentenceInst.chunkList)
-		SentenceInstList.append(SentenceInst)
-	return SentenceInstList
+		sentenceInstList.append(sentenceInst)
+		sentenceNum+=1
+	return (sentenceInstList,globalWordList)
