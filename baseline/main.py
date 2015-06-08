b6c298026da14b3741680ac6565598ab700b1d0d
aarjay00 <jain.rohit625@gmail.com>
2015-06-09 04:06:01 +0530

    merging nearing completion

--- baseline/main.py
+++ baseline/main.py
@@ -54,7 +54,11 @@ class discourseFile():
 				continue
 			print relation.relationType
 			print "arg1",relation.arg1Span
+			printSpan(relation.arg1Span,self.rawData)
+			getSpanFromAnn(relation.arg1Span,self.rawData,self.globalWordList,self.annToRawMapping,self.rawToAnnMapping)
 			print "connective",relation.connSpan
+			printSpan(relation.connSpan,self.rawData)
+			getSpanFromAnn(relation.connSpan,self.rawData,self.globalWordList,self.annToRawMapping,self.rawToAnnMapping)
 			print "arg2",relation.arg2Span
 #			span=convertSpan(relation.arg2Span,self.rawData,self.globalWordList)
 			printSpan(relation.arg2Span,self.rawData)
@@ -74,25 +78,26 @@ def mappingBetweenFiles(wordList,rawData):
 	while(1):
 		if(rawPos>=len(rawData) or wordPos>=len(wordList)):
 			break
-		print "comparing",rawData[rawPos],wordList[wordPos].word
-		print "comparing",repr(rawData[rawPos]),repr(wordList[wordPos].word)
+#		print "comparing",rawData[rawPos],wordList[wordPos].word
+#		print "comparing",repr(rawData[rawPos]),repr(wordList[wordPos].word)
 		if(rawData[rawPos]=="NULL"):
+			rawToWord[rawPos]=(wordPos,)
 			rawPos+=1
 			continue
 		if(rawData[rawPos]==wordList[wordPos].word):
-			print "word matched perfectly",rawData[rawPos],wordList[wordPos].word
+#			print "word matched perfectly",rawData[rawPos],wordList[wordPos].word
 			rawToWord[rawPos]=(wordPos,)
 			wordToRaw[wordPos]=(rawPos,)
 			wordPos+=1
 		else:
 			if(wordPos+1  < len(wordList)  and  wordList[wordPos].word+wordList[wordPos+1].word==rawData[rawPos] ):
-				print "word 1 matched perfectly",rawData[rawPos],wordList[wordPos].word+wordList[wordPos+1].word
+#				print "word 1 matched perfectly",rawData[rawPos],wordList[wordPos].word+wordList[wordPos+1].word
 				rawToWord[rawPos]=(wordPos,wordPos+1)
 				wordToRaw[wordPos]=(rawPos,)
 				wordToRaw[wordPos+1]=(rawPos,)
 				wordPos+=2
 			elif(not isNum(rawData[rawPos]) and wordPos+1  < len(wordList)  and  wordList[wordPos].word[:len(wordList[wordPos].word)-1]+wordList[wordPos+1].word[:len(wordList[wordPos+1].word)-1]==rawData[rawPos] ):
-				print "word 2 matched perfectly",rawData[rawPos],wordList[wordPos].word[:len(wordList[wordPos].word)-1]+wordList[wordPos+1].word[:len(wordList[wordPos+1].word)-1]
+#				print "word 2 matched perfectly",rawData[rawPos],wordList[wordPos].word[:len(wordList[wordPos].word)-1]+wordList[wordPos+1].word[:len(wordList[wordPos+1].word)-1]
 #				print wordList[wordPos].word
 #				print wordList[wordPos+1].word
 				rawToWord[rawPos]=(wordPos,wordPos+1)
@@ -100,14 +105,14 @@ def mappingBetweenFiles(wordList,rawData):
 				wordToRaw[wordPos+1]=(rawPos,)
 				wordPos+=2
 			elif( wordPos+2 < len(wordList ) and (wordList[wordPos].word+wordList[wordPos+2].word==rawData[rawPos] or wordList[wordPos].word+u'\u093e'+wordList[wordPos+2].word==rawData[rawPos])):
-				print "word 3 matched perfectly",rawData[rawPos],wordList[wordPos].word+wordList[wordPos+2].word, "extra",wordList[wordPos+1].word
+#				print "word 3 matched perfectly",rawData[rawPos],wordList[wordPos].word+wordList[wordPos+2].word, "extra",wordList[wordPos+1].word
 				rawToWord[rawPos]=(wordPos,wordPos+1,wordPos+2)
 				wordToRaw[wordPos]=(rawPos,)
 				wordToRaw[wordPos+1]=(rawPos,)
 				wordToRaw[wordPos+2]=(rawPos,)
 				wordPos+=3
 			elif (rawPos+2 < len(rawData) and rawData[rawPos]+rawData[rawPos+1]+rawData[rawPos+2]==wordList[wordPos].word):
-				print "word 4 matched perfectly"
+#				print "word 4 matched perfectly"
 				rawToWord[rawPos]=(wordPos,)
 				rawToWord[rawPos+1]=(wordPos,)
 				rawToWord[rawPos+2]=(wordPos,)
@@ -115,7 +120,7 @@ def mappingBetweenFiles(wordList,rawData):
 				rawPos+=2
 				wordPos+=1
 			elif(rawPos +4 < len(rawData) and rawData[rawPos]+rawData[rawPos+2]+rawData[rawPos+4]==wordList[wordPos].word):
-				print "word 5 matched perfectly"
+#				print "word 5 matched perfectly"
 				rawToWord[rawPos]=(wordPos,)
 				rawToWord[rawPos+1]=(wordPos,)
 				rawToWord[rawPos+2]=(wordPos,)
@@ -125,31 +130,31 @@ def mappingBetweenFiles(wordList,rawData):
 				rawPos+=4
 				wordPos+=1
 			elif(rawPos+1< len(rawData) and rawData[rawPos]+rawData[rawPos+1]==wordList[wordPos].word):
-				print "word 6 matched perfectly"
+#				print "word 6 matched perfectly"
 				rawToWord[rawPos]=(wordPos,)
 				rawToWord[rawPos+1]=(wordPos,)
 				wordToRaw[wordPos]=(rawPos,rawPos+1)
 				rawPos+=1
 				wordPos+=1
 			elif(rawData[rawPos].replace(u'\u0902',u'\u0901')==wordList[wordPos].word or rawData[rawPos].replace(u'\u0901',u'\u0902')==wordList[wordPos].word):
-				print "word 7 matched perfectly",rawData[rawPos],wordList[wordPos].word
+#				print "word 7 matched perfectly",rawData[rawPos],wordList[wordPos].word
 				rawToWord[rawPos]=(wordPos,)
 				wordToRaw[wordPos]=(rawPos,)
 				wordPos+=1
 			elif(rawPos +2 < len(rawData) and rawData[rawPos+2]==wordList[wordPos+1].word):
-				print "ahem ahem"
+#				print "ahem ahem"
 				rawToWord[rawPos]=(wordPos,)
 				rawToWord[rawPos+1]=(wordPos,)
 				wordToRaw[wordPos]=(rawPos,rawPos+1)
 				rawPos+=1
 				wordPos+=1
 			elif(wordPos +1 < len(wordList) and rawPos+1 < len(rawData) and wordList[wordPos+1].word==rawData[rawPos+1]):
-				print "-"*30,"SKIPPED 1",rawData[rawPos],wordList[wordPos].word
+#				print "-"*30,"SKIPPED 1",rawData[rawPos],wordList[wordPos].word
 				rawToWord[rawPos]=(wordPos,)
 				wordToRaw[wordPos]=(rawPos,)
 				wordPos+=1
 			elif(wordPos+2 < len(wordList) and rawPos+1< len(rawData) and rawData[rawPos+1]==wordList[wordPos+1].word+wordList[wordPos+2].word):
-				print "-"*30,"SKIPPED 2",rawData[rawPos],wordList[wordPos].word
+#				print "-"*30,"SKIPPED 2",rawData[rawPos],wordList[wordPos].word
 				rawToWord[rawPos]=(wordPos,)
 				wordToRaw[wordPos]=(rawPos,)
 				rawToWord[rawPos+1]=(wordPos+1,wordPos+2)
@@ -159,7 +164,7 @@ def mappingBetweenFiles(wordList,rawData):
 				rawPos+=1
 
 			elif(wordPos+2 < len(wordList) and rawPos + 2< len(rawData) and rawData[rawPos+2]==wordList[wordPos+2].word):
-				print "-"*30,"SKIPPED 3"
+#				print "-"*30,"SKIPPED 3"
 				rawToWord[rawPos]=(wordPos,)
 				wordToRaw[wordPos]=(rawPos,)
 				wordPos+=1
@@ -168,13 +173,13 @@ def mappingBetweenFiles(wordList,rawData):
 				wordToRaw[wordPos]=(rawPos,)
 				wordPos+=1
 			elif(wordPos +2 < len(wordList) and rawPos+1 < len(rawData) and wordList[wordPos+2].word==rawData[rawPos+1]):
-				print "-"*30,"SKIPPED 4",rawData[rawPos],wordList[wordPos].word
+#				print "-"*30,"SKIPPED 4",rawData[rawPos],wordList[wordPos].word
 				rawToWord[rawPos]=(wordPos,wordPos+1)
 				wordToRaw[wordPos]=(rawPos,)
 				wordToRaw[wordPos+1]=(rawPos,)
 				wordPos+=2
 			elif (wordPos+2 < len(wordList) and rawData[rawPos]==wordList[wordPos+2].word):
-				print "-"*30,"SKIPPED 5"
+#				print "-"*30,"SKIPPED 5"
 				rawToWord[rawPos-1]=list(rawToWord[rawPos-1])
 				rawToWord[rawPos-1].append(wordPos)
 				rawToWord[rawPos-1].append(wordPos+1)
@@ -184,7 +189,7 @@ def mappingBetweenFiles(wordList,rawData):
 				rawToWord[rawPos]=(wordPos+2,)
 				wordPos+=3
 			elif(rawPos+2 < len(rawData) and isNum(rawData[rawPos]) and isNum(rawData[rawPos+2]) and rawData[rawPos+1]=="," and (rawData[rawPos]+","+rawData[rawPos+2]==wordList[wordPos] or convertNum(rawData[rawPos])+"," +convertNum(rawData[rawPos+2]) == wordList[wordPos].word)):
-				print "number detected"
+#				print "number detected"
 				rawToWord[rawPos]=(wordPos,)
 				rawToWord[rawPos+1]=(wordPos,)
 				rawToWord[rawPos+2]=(wordPos,)
@@ -192,7 +197,7 @@ def mappingBetweenFiles(wordList,rawData):
 				rawPos+=2
 				wordPos+=1
 			elif(rawPos+4 < len(rawData) and isNum(rawData[rawPos]) and isNum(rawData[rawPos+2]) and isNum(rawData[rawPos+4]) and rawData[rawPos+1]=="," and rawData[rawPos+3]=="," and ( convertNum(rawData[rawPos])+","+convertNum(rawData[rawPos+2]) +","+ convertNum(rawData[rawPos+4]) == wordList[wordPos].word or rawData[rawPos]+","+rawData[rawPos+2]+","+rawData[rawPos+4]==wordList[wordPos].word)):
-				print "large number detected"
+#				print "large number detected"
 				rawToWord[rawPos]=(wordPos,)
 				rawToWord[rawPos+1]=(wordPos,)
 				rawToWord[rawPos+2]=(wordPos,)
@@ -215,9 +220,9 @@ def mappingBetweenFiles(wordList,rawData):
 					loop+=1
 #				print rawData[rawPos+2],wordList[wordPos+2].word
 				return (rawToWord,wordToRaw)
-		print rawPos
+#		print rawPos
 		rawPos+=1
-	print "stats %d %d"%(rawPos,wordPos)
+#	print "stats %d %d"%(rawPos,wordPos)
 	return (rawToWord,wordToRaw)
 
 #	def addUnit(self,discourseUnit):
@@ -232,12 +237,15 @@ def getStartPos(rawData,wordList):
 		startString=startString+" "+wordList[i].word
 #	print startString
 	startDoc=rawData.find(startString)
+#	print startDoc
 	if(startDoc==-1):	
 #		print "here here 1",wordList[0].word+" "+wordList[1].word+" "+wordList[2].word+" "+wordList[3].word
+#		print startDoc
 		startDoc=rawData.find(wordList[0].word+" "+wordList[1].word+" "+wordList[2].word+" "+wordList[3].word)
 	if(startDoc==-1):
 		startDoc=rawData.find(wordList[0].word+" "+wordList[1].word)
 #		print "here here 2",wordList[0].word+" "+wordList[1].word
+#		print startDoc
 	if(startDoc==-1):
 #		print "here here 3"
 		startDoc=rawData.find(wordList[1].word+" "+wordList[2].word)
@@ -255,6 +263,11 @@ def getStartPos(rawData,wordList):
 		while(rawData[startDoc]!=' '):
 			startDoc-=1
 #		print startDoc
+	if(startDoc<0):
+#		print "here here 5"
+		startDoc=rawData.find(wordList[0].word+wordList[1].word+wordList[2].word)
+#		print startDoc
+#	print "getting startDoc ",startDoc
 	return startDoc
 
 def editDistance(word1 , word2):
@@ -307,7 +320,7 @@ def printSpan(spans,rawData):
 	spans=re.split(';',spans)
 	for span in spans:
 		span=re.split("\.\.",span)
-		print span
+#		print span
 		print rawData[int(span[0]):int(span[1])],"-"
 def printSpanWord(spans,wordList):
 	for span in spans:
@@ -323,21 +336,18 @@ def getSpanFromAnn(spans,rawData,wordList,annToWord,wordToAnn):
 	returnSpan=[]
 	for span in spans:
 		span=re.split("\.\.",span)
+#		print startDoc
 #		print rawData[int(span[0]):int(span[1])]
-		dataBefore=rawData[startDoc:int(span[0])]
+		dataBefore=rawData[startDoc:int(span[0])-1]
 		dataAfter=rawData[startDoc:int(span[1])]
 		dataBefore=addSpaces(dataBefore).split()
 		dataAfter=addSpaces(dataAfter).split()
-		print len(dataBefore)
-		print len(dataAfter)
-		print wordToAnn[len(dataBefore)]
-		print wordToAnn[len(dataAfter)]
+#		print len(dataBefore),
+#		print len(dataAfter),
+#		print wordToAnn[len(dataBefore)],
+#		print wordToAnn[len(dataAfter)-1]
 		for wordNum in range(len(dataBefore),len(dataAfter)):
-			try:
-				wordNumTuple=wordToAnn[wordNum]
-			except:
-#				print dataAfter[wordNum]
-				wordNumTuple=wordToAnn[wordNum]
+			wordNumTuple=wordToAnn[wordNum]
 			for word in wordNumTuple:
 				print wordList[word].word,
 		print "\n"
