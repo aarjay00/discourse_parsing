c0fccb850e33ec1f95fd0eded55e2f8b03a77640
Rohit Jain <aarjay@Rohits-MacBook-Pro-5.local>
2017-04-29 13:28:59 +0530

    updated keras exp

--- baseline/keras_experiments.py
+++ baseline/keras_experiments.py
@@ -38,6 +38,32 @@ def connective_model():
     model.add(Dense(512, input_dim=286, activation='relu'))
     model.add(Dense(256, activation='relu'))
     model.add(Dense(128, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(8,activation="relu"))
+    model.add(Dense(1, activation='sigmoid'))
+
+    # Compile model
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    # Fit the model
+    model.fit(X, Y, epochs=500, batch_size=10)
+    # evaluate the model
+    scores = model.evaluate(X, Y)
+    print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+    model.save("connective_deep_4")
+
+def connective_rnn():
+    # load pima indians dataset
+    dataset = numpy.loadtxt("connective-features.csv", delimiter=",")
+    # split into input (X) and output (Y) variables
+    X = dataset[:, 0:286]
+    Y = dataset[:, 286]
+
+    # create model
+    model = Sequential()
+    model.add(Dense(1024, input_dim=286, activation='relu'))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(128, activation='relu'))
     model.add(Dense(64, activation='relu'))
     model.add(Dense(32, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
@@ -49,5 +75,6 @@ def connective_model():
     # evaluate the model
     scores = model.evaluate(X, Y)
     print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
-    model.save("connective_deep")
+    model.save("connective_rnn")
+
 connective_model()
